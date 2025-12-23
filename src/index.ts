01c06b4629e8bd813751785594eb9a0a0567bc9c
Jiayang Chen <Jiayangc1@gmail.com>
2025-12-23 15:19:03 -0600

    Add debug output for ROLE_IDS
    Added debug output for ROLE_IDS environment variable.

--- src/index.ts
+++ src/index.ts
@@ -22,6 +22,7 @@ const client = new Client({
 // Add your member roles to this array
 export const memberRoles = JSON.parse(process.env.ROLE_IDS) as string[];
 console.log("DEBUG OUTPUT:", process.env.ROLE_IDS);
+//debug
 // Add your flagged alt account roles to this array. Comment this line out if you're not using it. Make sure to also comment out the grantRole function that grants this role.
 const altRole = process.env.ALT_ROLE_ID;
 
